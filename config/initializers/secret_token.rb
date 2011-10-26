f8efbea4ac199c87d946c1e68f6349bfbfac22ef
Scott Cytacki <scytacki@concord.org>
2011-10-26 16:22:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SgAuthoring::Application.config.secret_token = '8bc0d8904761f097b45258cbe96d433607223814ce55268240695072a434469f12bf4de3f677c375e1edc19fde6e8c467014373fdfa4cfed2fe08e6ca0980097'
