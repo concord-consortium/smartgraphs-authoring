7c1ca2268201acd52d928ba5017c5bf1e38ed3a4
Scott Cytacki <scytacki@concord.org>
2011-10-28 11:00:41 -0400

    fix path in to index.html

--- app/controllers/json_activities_controller.rb
+++ app/controllers/json_activities_controller.rb
@@ -7,7 +7,7 @@ class JsonActivitiesController < ApplicationController
   auto_actions :all
 
   def preview
-    File.open "#{Rails.root}/public/static/smartgraphs/en/82b404e9816653aae3437852c272301c88eb986a/index.html" do |file|
+    File.open "#{Rails.root}/public/static/smartgraphs/en/817dd27df90f8bee344663d0bd63f4993512bbd2/index.html" do |file|
       template = ERB.new file.read
       authored_activity_json = JsonActivity.find(params[:id]).json
       render :text => template.result(binding)
