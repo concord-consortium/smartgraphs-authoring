150c935bfa436ae80b5b6f2b3d6c02b89ab692b1
Noah Paessel <knowuh@gmail.com>
2011-10-28 15:33:28 -0400

    preview activity calls converter.new().convert(a)

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -18,7 +18,10 @@ class ActivitiesController < ApplicationController
 
   def preview
     File.open "#{Rails.root}/public/static/smartgraphs/en/5a2301d099b8d537c51560051dd2bc99eb4b582f/index.html" do |file|
-      render :text => file.read
+      template = ERB.new file.read
+      authored_activity_json = Converter.new("sg-convert").convert(Activity.find(params[:id]).to_json)
+      # authored_activity_json = Converter.new("sg-convert").convert("{}")
+      render :text => template.result(binding)
     end
   end
 
