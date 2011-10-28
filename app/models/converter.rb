f0d45fff66305b5abb0f29dd254226b5fee17798
Noah Paessel <knowuh@gmail.com>
2011-10-28 16:40:36 -0400

    trying to be more mindful of the pipe status.

--- app/models/converter.rb
+++ app/models/converter.rb
@@ -1,7 +1,7 @@
 # Dummy class to wrap external json conversion process
 # in this first instance we are just shelling out to cat.
 class Converter
-    attr_accessor :converter_call
+  attr_accessor :converter_call
 
   # initialize(path to binary, array of CLI args)
   def initialize(call="/bin/cat")
@@ -12,10 +12,15 @@ class Converter
   # shell out through process pipe and return STDOUT of
   # process
   def convert(string)
+    retval = "{}"
     IO.popen(self.converter_call, 'r+') do |pipe|
       pipe.puts(string)
       pipe.close_write
-      return pipe.read.chomp
+      pipe.flush
+      retval= pipe.read
+      pipe.flush
+      pipe.close_read
     end
+    return retval.chomp
   end
 end
