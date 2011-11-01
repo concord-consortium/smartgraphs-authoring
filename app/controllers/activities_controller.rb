e0a3aa0bfd7966eb5ce924a000c170ae034712cb
Scott Cytacki <scytacki@concord.org>
2011-11-01 15:18:36 -0400

    add json view of generated sg_runtime son

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -16,11 +16,21 @@ class ActivitiesController < ApplicationController
     end
   end
 
-  show_action :preview do 
-    authored_activity_json = Converter.new().convert(Activity.find(params[:id]).to_hash.to_json)
-    File.open "#{Rails.root}/public/static/smartgraphs/en/5a2301d099b8d537c51560051dd2bc99eb4b582f/index.html" do |file|
-      template = ERB.new file.read
-      render :text => template.result(binding)
+  show_action :preview do
+    hobo_show do |format|
+      sg_runtime_json = Converter.new().convert(Activity.find(params[:id]).to_hash.to_json)
+      format.json {
+        render :text => JSON.pretty_generate(JSON.parse(sg_runtime_json))
+      }
+      format.html {
+        File.open "#{Rails.root}/public/static/smartgraphs/en/5a2301d099b8d537c51560051dd2bc99eb4b582f/index.html" do |file|
+          # the 'authored_activity_json' variable name is used in the template so can't be changed without
+          # changing the template
+          authored_activity_json = sg_runtime_json
+          template = ERB.new file.read
+          render :text => template.result(binding)
+        end
+      }
     end
   end
 
