56f33f59251f22d443ecd885ae6b3bb9e8d9e397
Scott Cytacki <scytacki@concord.org>
2011-11-06 00:24:55 -0400

    dry up the customized 'owner' controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  class << self
+
+    def polymorphic_auto_actions_for(owner_association, actions)
+      auto_actions_for owner_association, actions
+
+      define_method "new_for_#{owner_association}" do
+        owner, association = find_owner_and_association(owner_association)
+        new_record = association.new
+        new_record.send "#{owner_association}=", owner
+        hobo_new_for owner_association, new_record
+      end
+
+      define_method "create_for_#{owner_association}" do
+        owner, association = find_owner_and_association(owner_association)
+        attributes = attribute_parameters
+        attributes[owner_association] = owner
+        hobo_create_for owner_association, :attributes => attributes
+      end
+    end
+  end
 end
