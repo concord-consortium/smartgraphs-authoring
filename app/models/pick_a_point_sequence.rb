ddd48658a66f40c8f147c98d2460790991e2047e
Aaron Unger <aaron.unger@gmail.com>
2011-11-09 09:44:59 -0800

    give up and correct hints are now just static text.

--- app/models/pick_a_point_sequence.rb
+++ app/models/pick_a_point_sequence.rb
@@ -3,10 +3,12 @@ class PickAPointSequence < ActiveRecord::Base
   hobo_model # Don't put anything above this
 
   fields do
-    title          :string
-    initialPrompt  :text
-    correctAnswerX :float
-    correctAnswerY :float
+    title            :string
+    initial_prompt   :text
+    correct_answer_x :float
+    correct_answer_y :float
+    give_up          :text
+    confirm_correct  :text
     timestamps
   end
 
@@ -19,8 +21,8 @@ class PickAPointSequence < ActiveRecord::Base
   has_many :text_hints, :through => :sequence_hints, :source => :hint, :source_type => 'TextHint'
   reverse_association_of :text_hints, 'TextHint#pick_a_point_sequence'
 
-#  belongs_to :give_up_hint, :polymorphic => true, :index => 'index_give_up_hints'
-#  belongs_to :confirm_correct_hint, :polymorphic => true, :index => 'index_confirm_correct_hints'
+#  belongs_to :give_up_hint, :index => 'index_give_up_hints'
+#  belongs_to :confirm_correct_hint, :index => 'index_confirm_correct_hints'
 
   children :sequence_hints
 
--- db/schema.rb
+++ db/schema.rb
@@ -77,19 +77,14 @@ ActiveRecord::Schema.define(:version => 20111108185104) do
 
   create_table "pick_a_point_sequences", :force => true do |t|
     t.string   "title"
-    t.text     "initialPrompt"
-    t.float    "correctAnswerX"
-    t.float    "correctAnswerY"
+    t.text     "initial_prompt"
+    t.float    "correct_answer_x"
+    t.float    "correct_answer_y"
+    t.text     "give_up"
+    t.text     "confirm_correct"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "give_up_hint_id"
-    t.string   "give_up_hint_type"
-    t.integer  "confirm_correct_hint_id"
-    t.string   "confirm_correct_hint_type"
   end
-
-  add_index "pick_a_point_sequences", ["confirm_correct_hint_type", "confirm_correct_hint_id"], :name => "index_confirm_correct_hints"
-  add_index "pick_a_point_sequences", ["give_up_hint_type", "give_up_hint_id"], :name => "index_give_up_hints"
 
   create_table "predefined_graph_panes", :force => true do |t|
     t.string   "title"
