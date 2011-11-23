290b60ddbcbca3a8dcf8f15c7dce339414506f6b
Aaron Unger <aaron.unger@gmail.com>
2011-11-23 13:34:49 -0800

    Real production deploy config.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,12 +8,12 @@ set :gateway, "otto.concord.org"
 #############################################################
 
 set :deploy_to, "/web/portal"
-set :branch, "master"
+set :branch, "production"
 
 #############################################################
 #  Servers
 #############################################################
 
-set :domain, "ruby-vm8.concord.org"
+set :domain, "ruby-vm10.concord.org"
 server domain, :app, :web
 role :db, domain, :primary => true
