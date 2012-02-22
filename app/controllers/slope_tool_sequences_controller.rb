350af2b5802154e874e22a78a8df6d66223976bf
Noah Paessel <knowuh@gmail.com>
2012-02-22 13:40:48 -0500

    Initial skeleton of SlopeToolSequence

---
+++ app/controllers/slope_tool_sequences_controller.rb
@@ -0,0 +1,7 @@
+class SlopeToolSequencesController < ApplicationController
+
+  hobo_model_controller
+
+  auto_actions :all
+
+end
---
+++ spec/models/slope_tool_sequence_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe SlopeToolSequence do
+  describe "case_type field validation" do
+    it "should require case_type to be one of A B or C" do
+      slope_tool = SlopeToolSequence.new(:case_type => "A")
+      slope_tool.should be_valid
+      slope_tool = SlopeToolSequence.new(:case_type => true)
+      slope_tool.errors.should_not be_nil
+    end
+  end
+  describe "first_question_is_slope_question" do
+    it "should return true for cases A and C" do
+      slope_tool = SlopeToolSequence.new(:case_type => "A")
+      slope_tool.first_question_is_slope_question.should == true
+      slope_tool = SlopeToolSequence.new(:case_type => "C")
+      slope_tool.first_question_is_slope_question.should == true
+    end
+    it "should return false for case B" do
+      slope_tool = SlopeToolSequence.new(:case_type => "B")
+      slope_tool.first_question_is_slope_question.should == false
+    end
+  end
+  describe "student_selects_points" do
+    it "should return true for cases A and B" do
+      slope_tool = SlopeToolSequence.new(:case_type => "A")
+      slope_tool.student_selects_points.should == true
+      slope_tool = SlopeToolSequence.new(:case_type => "B")
+      slope_tool.student_selects_points.should == true
+    end
+    it "should return false for case C" do
+      slope_tool = SlopeToolSequence.new(:case_type => "C")
+      slope_tool.student_selects_points.should == false
+    end
+  end
+end
