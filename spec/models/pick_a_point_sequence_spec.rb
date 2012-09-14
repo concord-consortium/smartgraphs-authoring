6c3a73e8d3d5edfe7a53ac02920e2531626b0358
Noah Paessel <knowuh@gmail.com>
2012-09-14 15:14:19 -0400

    Set nil in to_hash, so json will have null literals (for generator).

--- spec/models/pick_a_point_sequence_spec.rb
+++ spec/models/pick_a_point_sequence_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe PickAPointSequence do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "#to_hash" do
+    before(:each) do
+      params = {
+        :title  => "pick a point",
+        :initial_prompt  => "where is the midpoint",
+        :give_up => "sorry",
+        :confirm_correct => "correct",  
+        :correct_answer_x_min => 0.3  
+      }
+      @sequence = PickAPointSequence.new(params) 
+    end
+  
+    describe "json format" do
+      it "should include unquoted null values" do
+        @sequence.correct_answer_x_min.should == 0.3
+        json = JSON.pretty_generate(JSON.parse(@sequence.to_hash.to_json))
+        json.should match(/null/)
+        json.should_not match (/"null"/)
+      end
+    end
+  end
+
 end
