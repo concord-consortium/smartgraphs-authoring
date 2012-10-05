2fec7394da7edbffff23dc1bd13d36765ed96d48
Noah Paessel <knowuh@gmail.com>
2012-10-05 14:16:40 -0400

    Migration bug might be sqlite3 only. testing.
    Revert "[#37246813] Fix migrations preventing jenkins from running tests."
    
    This reverts commit f941fa1c2102642dc49646487c141d04a8d8623f.

--- db/migrate/20111110225115_hobo_migration_19.rb
+++ db/migrate/20111110225115_hobo_migration_19.rb
@@ -6,11 +6,11 @@ class HoboMigration19 < ActiveRecord::Migration
 
     add_column :initial_prompt_prompts, :numeric_sequence_id, :integer
 
-    add_index :confirm_correct_prompts, [:numeric_sequence_id], :name => 'indx_confirm_correct_numeric_seq_id'
+    add_index :confirm_correct_prompts, [:numeric_sequence_id]
 
-    add_index :give_up_prompts, [:numeric_sequence_id], :name => 'indx_give_up_numeric_seq_id'
+    add_index :give_up_prompts, [:numeric_sequence_id]
 
-    add_index :initial_prompt_prompts, [:numeric_sequence_id], :name => 'indx_initial_prompt_numeric_seq_id'
+    add_index :initial_prompt_prompts, [:numeric_sequence_id]
   end
 
   def self.down
@@ -20,10 +20,10 @@ class HoboMigration19 < ActiveRecord::Migration
 
     remove_column :initial_prompt_prompts, :numeric_sequence_id
 
-    remove_index :confirm_correct_prompts, :name => :indx_confirm_correct_numeric_seq_id rescue ActiveRecord::StatementInvalid
+    remove_index :confirm_correct_prompts, :name => :index_confirm_correct_prompts_on_numeric_sequence_id rescue ActiveRecord::StatementInvalid
 
-    remove_index :give_up_prompts, :name => :indx_give_up_numeric_seq_id rescue ActiveRecord::StatementInvalid
+    remove_index :give_up_prompts, :name => :index_give_up_prompts_on_numeric_sequence_id rescue ActiveRecord::StatementInvalid
 
-    remove_index :initial_prompt_prompts, :name => :indx_initial_prompt_numeric_seq_id rescue ActiveRecord::StatementInvalid
+    remove_index :initial_prompt_prompts, :name => :index_initial_prompt_prompts_on_numeric_sequence_id rescue ActiveRecord::StatementInvalid
   end
 end
