ed6f388d50b5f6ac6c95de1b7b37ef3105076e3e
Noah Paessel <knowuh@gmail.com>
2012-10-05 13:52:50 -0400

    [#37246813] Fix migrations preventing jenkins from running tests.
    https://www.pivotaltracker.com/story/show/37246813
    Fix problem preventing jenkins from running tests.
    
    Autogenerated index names were too long in migrations > 64 char.
    
    see:
    https://github.com/rails/rails/pull/1993/files
    
    + bundle exec rake db:migrate
    rake aborted!
    An error has occurred, this and all later migrations canceled:
    
    Index name 'temp_index_altered_confirm_correct_prompts_on_pick_a_point_sequence_id' on table 'altered_confirm_correct_prompts' is too long; the limit is 64 characters

--- db/migrate/20111110172132_hobo_migration_16.rb
+++ db/migrate/20111110172132_hobo_migration_16.rb
@@ -7,8 +7,8 @@ class HoboMigration16 < ActiveRecord::Migration
       t.integer  :prompt_id
       t.string   :prompt_type
     end
-    add_index :confirm_correct_prompts, [:pick_a_point_sequence_id]
-    add_index :confirm_correct_prompts, [:prompt_type, :prompt_id], :name => 'index_confirm_correct_prompts'
+    add_index :confirm_correct_prompts, [:pick_a_point_sequence_id], :name => 'index_confirm_correct_pick_a_points'
+    add_index :confirm_correct_prompts, [:prompt_type, :prompt_id],  :name => 'index_confirm_correct_prompts'
 
     create_table :give_up_prompts do |t|
       t.datetime :created_at
@@ -17,8 +17,8 @@ class HoboMigration16 < ActiveRecord::Migration
       t.integer  :prompt_id
       t.string   :prompt_type
     end
-    add_index :give_up_prompts, [:pick_a_point_sequence_id]
-    add_index :give_up_prompts, [:prompt_type, :prompt_id], :name => 'index_give_up_prompts'
+    add_index :give_up_prompts, [:pick_a_point_sequence_id], :name  => 'index_give_up_pick_a_points'
+    add_index :give_up_prompts, [:prompt_type, :prompt_id],  :name  => 'index_give_up_prompts'
 
     create_table :initial_prompt_prompts do |t|
       t.datetime :created_at
@@ -27,7 +27,7 @@ class HoboMigration16 < ActiveRecord::Migration
       t.integer  :prompt_id
       t.string   :prompt_type
     end
-    add_index :initial_prompt_prompts, [:pick_a_point_sequence_id]
+    add_index :initial_prompt_prompts, [:pick_a_point_sequence_id], :name => 'index_initial_prompt_pick_a_points'
     add_index :initial_prompt_prompts, [:prompt_type, :prompt_id], :name => 'index_initial_prompt_prompts'
   end
 
