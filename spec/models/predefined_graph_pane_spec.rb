825c03a2bf9e5886572b89b7afb15e16ac4b4b74
Noah Paessel <knowuh@gmail.com>
2012-11-28 18:46:55 -0500

    Update spec tests for multiple DataSet GraphPanes.

--- spec/models/predefined_graph_pane_spec.rb
+++ spec/models/predefined_graph_pane_spec.rb
@@ -1,55 +1,10 @@
 require 'spec_helper'
 
 describe PredefinedGraphPane do
-  describe "#normalize_data" do
-    it "should run on empty graph" do
-      empty_data = ""
-      subject.normalize_data
-      subject.data.should match empty_data
-    end
-    it "should return comma separated data" do
-      subject.data = "1,2\n1,3"
-      subject.normalize_data
-      subject.data.should == "1,2\n1,3"
-    end
-    it "tabs should be replaced by comma separated data" do
-      subject.data = "1\t2\n1\t3"
-      subject.normalize_data
-      subject.data.should == "1,2\n1,3"
-    end
-    it "extra lines should be stripped" do
-      subject.data = "\n\n1,2\n1,3\n\n"
-      subject.normalize_data
-      subject.data.should == "1,2\n1,3"
-    end
-    it "extra space should be stripped" do
-      subject.data = "1 , 2  \n 1 , 3"
-      subject.normalize_data
-      subject.data.should == "1,2\n1,3"
-    end
-  end
 
-  describe "#normalize_expression" do
-    it "should get rid of the left side of y =" do
-      subject.expression="y = 23 * x + 4"
-      subject.normalize_expression
-      subject.expression.should == "23 * x + 4"
-    end
-    it "should leave well enough alone" do
-      subject.expression="22 * x + 6"
-      subject.normalize_expression
-      subject.expression.should == "22 * x + 6"
-    end
-    it "should leave non-standard expressions alone, to fail validation later" do
-      subject.expression="z + y = 22 * x + 6"
-      subject.normalize_expression
-      subject.expression.should == "z + y = 22 * x + 6"
-    end
-
-  end
   describe "#to_hash" do
     let(:expected_hash) { 
-      {"type"=>"PredefinedGraphPane", "title"=>nil, "yLabel"=>nil, "yUnits"=>nil, "yMin"=>nil, "yMax"=>nil, "xLabel"=>nil, "xUnits"=>nil, "xMin"=>nil, "xMax"=>nil, "yTicks"=>nil, "xTicks"=>nil, "xPrecision"=>0.1, "yPrecision"=>0.1, "data"=>[], "expression"=>"", "lineSnapDistance"=>0.1, "lineType"=>"none", "pointType"=>"dot", "showCrossHairs"=>false, "showToolTipCoords"=>false, "showGraphGrid"=>false}
+      {"type"=>"PredefinedGraphPane", "title"=>nil, "yLabel"=>nil, "yMin"=>nil, "yMax"=>nil, "xLabel"=>nil, "xMin"=>nil, "xMax"=>nil, "yTicks"=>nil, "xTicks"=>nil, "showCrossHairs"=>false, "showToolTipCoords"=>false, "showGraphGrid"=>false, "includedDataSets"=>[]} 
     }  
     it "should match our expected hash" do
       subject.to_hash.should == expected_hash
@@ -72,11 +27,12 @@ describe PredefinedGraphPane do
       :expression => "22 * x - 3",
       :data => "1,1,2,2,3,3,4,4"
       })}
+
     
     describe "#to_hash" do
       let(:expected_hash) { 
-        {"type"=>"PredefinedGraphPane", "title"=>"x in terms of y", "yLabel"=>"y", "yUnits"=>nil, "yMin"=>0.0, "yMax"=>1.0, "xLabel"=>"x", "xUnits"=>nil, "xMin"=>0.0, "xMax"=>10.0, "yTicks"=>0.1, "xTicks"=>1.0, "xPrecision"=>0.1, "yPrecision"=>0.1, "data"=>[[1.0, 1.0, 2.0, 2.0, 3.0, 3.0, 4.0, 4.0]], "expression"=>"y  = 22 * x - 3", "lineSnapDistance"=>0.1, "lineType"=>"none", "pointType"=>"dot", "showCrossHairs"=>false, "showToolTipCoords"=>false, "showGraphGrid"=>false}
-      }  
+        {"type"=>"PredefinedGraphPane", "title"=>"x in terms of y", "yLabel"=>"y", "yMin"=>0.0, "yMax"=>1.0, "xLabel"=>"x", "xMin"=>0.0, "xMax"=>10.0, "yTicks"=>0.1, "xTicks"=>1.0, "showCrossHairs"=>false, "showToolTipCoords"=>false, "showGraphGrid"=>false, "includedDataSets"=>[]}
+      }
       it "should match our expected hash" do
         subject.to_hash.should == expected_hash
       end
@@ -97,95 +53,6 @@ describe PredefinedGraphPane do
   end
    
   describe "validations" do
-    describe "expression" do
-      describe "passing expressions" do
-        it "validates null expressions" do
-          test = PredefinedGraphPane.new(
-            :title     => "title",
-            :y_label   => "y",
-            :y_min     =>  0 ,
-            :y_max     =>  10,
-            :y_ticks    => 10,
-            :x_label     => "x",
-            :x_min       => 0,
-            :x_max       => 10,
-            :x_ticks     => 10,
-            :expression => "")
-          test.should be_valid
-        end
-        it "validates  y = m * x + b" do
-          test = PredefinedGraphPane.new(
-            :title     => "title",
-            :y_label   => "y",
-            :y_min     =>  0 ,
-            :y_max     =>  10,
-            :y_ticks    => 10,
-            :x_label     => "x",
-            :x_min       => 0,
-            :x_max       => 10,
-            :x_ticks     => 10,
-            :expression => "0.3 * x + 4")
-          test.should be_valid
-          test = PredefinedGraphPane.new(
-            :title     => "title",
-            :y_label   => "y",
-            :y_min     =>  0 ,
-            :y_max     =>  10,
-            :y_ticks    => 10,
-            :x_label     => "x",
-            :x_min       => 0,
-            :x_max       => 10,
-            :x_ticks     => 10,
-            :expression => "y = x + 5")
-          test.should be_valid
-        end
-      end
-      describe "more complicated expression" do
-        it "passes pow(x,10) * (4.0 * atan( log(x) + 3))" do
-            test = PredefinedGraphPane.new(
-            :title     => "title",
-            :y_label   => "y",
-            :y_min     =>  0 ,
-            :y_max     =>  10,
-            :y_ticks    => 10,
-            :x_label     => "x",
-            :x_min       => 0,
-            :x_max       => 10,
-            :x_ticks     => 10,
-            :expression => "pow(x,10) * (4.0 * atan( log(x) + 3))")
-          test.should be_valid
-        end
-      end
-      describe "failing expressions" do
-        it "fails z = y + x + 3" do
-          test = PredefinedGraphPane.new(
-            :title     => "title",
-            :y_label   => "y",
-            :y_min     =>  0 ,
-            :y_max     =>  10,
-            :y_ticks    => 10,
-            :x_label     => "x",
-            :x_min       => 0,
-            :x_max       => 10,
-            :x_ticks     => 10,
-            :expression => "z = y + x + 3")
-          test.should_not be_valid
-        end
-        it "console.log('foo');" do
-          test = PredefinedGraphPane.new(
-            :title     => "title",
-            :y_label   => "y",
-            :y_min     =>  0 ,
-            :y_max     =>  10,
-            :y_ticks    => 10,
-            :x_label     => "x",
-            :x_min       => 0,
-            :x_max       => 10,
-            :x_ticks     => 10,
-            :expression => "console.log('foo');")
-          test.should_not be_valid
-        end
-      end
-    end
+    
   end
 end
--- spec/models/sensor_graph_pane_spec.rb
+++ spec/models/sensor_graph_pane_spec.rb
@@ -4,7 +4,7 @@ describe SensorGraphPane do
   describe "zero argument constructor defaults" do
     describe "#to_hash" do
       let(:expected_hash) { 
-        {"type"=>"SensorGraphPane", "title"=>nil, "yLabel"=>nil, "yUnits"=>nil, "yMin"=>nil, "yMax"=>nil, "xLabel"=>nil, "xUnits"=>nil, "xMin"=>nil, "xMax"=>nil, "yTicks"=>nil, "xTicks"=>nil }
+        {"type"=>"SensorGraphPane", "title"=>nil, "yLabel"=>nil, "yMin"=>nil, "yMax"=>nil, "xLabel"=>nil, "xMin"=>nil, "xMax"=>nil, "yTicks"=>nil, "xTicks"=>nil} 
       }  
       it "should match our expected hash" do
         subject.to_hash.should == expected_hash
@@ -27,7 +27,7 @@ describe SensorGraphPane do
 
     describe "#to_hash" do
       let(:expected_hash) { 
-       {"type"=>"SensorGraphPane", "title"=>"x in terms of y", "yLabel"=>"y", "yUnits"=>nil, "yMin"=>0.0, "yMax"=>1.0, "xLabel"=>"x", "xUnits"=>nil, "xMin"=>0.0, "xMax"=>10.0, "yTicks"=>0.1, "xTicks"=>1.0}
+        {"type"=>"SensorGraphPane", "title"=>"x in terms of y", "yLabel"=>"y", "yMin"=>0.0, "yMax"=>1.0, "xLabel"=>"x", "xMin"=>0.0, "xMax"=>10.0, "yTicks"=>0.1, "xTicks"=>1.0}
       }  
       it "should match our expected hash" do
         subject.to_hash.should == expected_hash
