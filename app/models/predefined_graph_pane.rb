8ee33a57308fa58fda97cd4b8fa8c5e7ab2a0269
Parker Morse <flashesofpanic@gmail.com>
2012-12-06 11:51:14 -0500

    Moving include_annotations_from_hash back to PredefinedGraphPane
    This was a refactoring error. [#37445533]

--- app/models/predefined_graph_pane.rb
+++ app/models/predefined_graph_pane.rb
@@ -72,6 +72,20 @@ class PredefinedGraphPane < ActiveRecord::Base
     'PredefinedGraphPane'
   end
 
+  def include_annotations_from_from_hash(graph_reference_urls)
+    self_ref = self
+    callback = Proc.new do
+      self_ref.reload
+      graphs = self_ref.sg_activity.pages.map { |p| [p.predefined_graph_panes,p.prediction_graph_panes,p.sensor_graph_panes]}
+      graphs.flatten!
+      graphs.select! { |g| (g.respond_to? :get_indexed_path) && (graph_reference_urls.include?(g.get_indexed_path))}
+      graphs.each do |graph|
+        self_ref.included_graphs << graph
+      end
+    end
+    self.add_marshall_callback(callback)
+  end
+
   def to_hash
     hash = super()
     hash["showCrossHairs"] = show_cross_hairs
