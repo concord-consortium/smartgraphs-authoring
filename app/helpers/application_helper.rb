1616109e31d3270dc644e723968863b06f5d6652
Parker Morse <flashesofpanic@gmail.com>
2012-12-17 15:21:48 -0500

    WIP: Scoping data_set selects to activities
    [#37445673] is the story that prompted this, but it applies to several others. We can use a Hobo scope to limit the data_sets available in the form, and for the most part this works with editing existing objects, but we're having trouble getting the activity to scope to when creating new objects.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def activity_for(something)
+    binding.pry
+    if params[:activity_id]
+      @activity_context = Activity.find(params[:activity_id])
+    elsif params[:page_id]
+      @activity_context = Page.find(params[:page_id]).activity
+    elsif something.respond_to?(:activity)
+      @activity_context = something.activity
+    else
+      @activity_context = nil
+    end
+  end
 end
