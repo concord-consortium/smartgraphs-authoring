2f7431b1ff4edb726767a290f4658490c8c90ed3
Parker Morse <flashesofpanic@gmail.com>
2012-12-17 21:41:18 -0500

    Connect Sequences to DataSets in existing Activities
    For [#40564705]. Adds to the migration which extracts DataSets from PredefinedGraphPanes and PredictionGraphPanes.

--- db/migrate/20121217211756_pull_data_into_datasets.rb
+++ db/migrate/20121217211756_pull_data_into_datasets.rb
@@ -1,6 +1,25 @@
 class PullDataIntoDatasets < ActiveRecord::Migration
   def self.up
+    # Pull data from PredefinedGraphPanes and PredictionGraphPanes into DataSets
     DataSet.convertAllGraphPanes();
+    # Link Sequences with datasets
+    # - Build array of sequences
+    sequences = NumericSequence.all
+    sequences << LineConstructionSequence.all
+    sequences << MultipleChoiceSequence.all
+    sequences << SlopeToolSequence.all
+    sequences.each do |seq|
+      # Check for predefined graph panes, prediction graph panes, and senor graph panes in that order
+      # Use their data set as the sequence data set if one is found.
+      if seq.page.predefined_graph_panes.length > 0
+        seq.data_set = seq.page.predefined_graph_panes.first.data_sets.first
+      elsif seq.page.prediction_graph_panes.length > 0
+        seq.data_set = seq.page.prediction_graph_panes.first.data_sets.first
+      elsif seq.page.sensor_graph_panes.length > 0
+        seq.data_set = seq.page.sensor_graph_panes.first.data_sets.first
+      end
+      seq.save
+    end
   end
 
   def self.down
