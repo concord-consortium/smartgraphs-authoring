530998ca00b29aaccc779c278ca077958338ff58
Parker Morse <flashesofpanic@gmail.com>
2012-12-19 12:00:53 -0500

    Helper method for getting activity from child

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,14 @@
 module ApplicationHelper
 
-  def activity_for(something)
-    binding.pry
+  def activity_for(child)
     if params[:activity_id]
       @activity_context = Activity.find(params[:activity_id])
     elsif params[:page_id]
       @activity_context = Page.find(params[:page_id]).activity
-    elsif something.respond_to?(:activity)
-      @activity_context = something.activity
+    elsif child.respond_to?(:activity)
+      @activity_context = child.activity
+    elsif child.respond_to?(:page)
+      @activity_context = child.page.activity
     else
       @activity_context = nil
     end
