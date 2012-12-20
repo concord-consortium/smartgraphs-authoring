f692d035ce4fb3fd345eff598818dc4c9246d6a5
Parker Morse <flashesofpanic@gmail.com>
2012-12-20 09:41:37 -0500

    Updated migration to allow for orphaned sequences.

--- db/migrate/20121217211756_pull_data_into_datasets.rb
+++ db/migrate/20121217211756_pull_data_into_datasets.rb
@@ -12,14 +12,18 @@ class PullDataIntoDatasets < ActiveRecord::Migration
     sequences.flatten.each do |seq|
       # Check for predefined graph panes, prediction graph panes, and senor graph panes in that order
       # Use their data set as the sequence data set if one is found.
-      if seq.page.predefined_graph_panes.length > 0
-        seq.data_set = seq.page.predefined_graph_panes.first.data_sets.first
-      elsif seq.page.prediction_graph_panes.length > 0
-        seq.data_set = seq.page.prediction_graph_panes.first.data_sets.first
-      elsif seq.page.sensor_graph_panes.length > 0
-        seq.data_set = seq.page.sensor_graph_panes.first.data_sets.first
+      if seq.page
+        if seq.page.predefined_graph_panes.length > 0
+          seq.data_set = seq.page.predefined_graph_panes.first.data_sets.first
+        elsif seq.page.prediction_graph_panes.length > 0
+          seq.data_set = seq.page.prediction_graph_panes.first.data_sets.first
+        elsif seq.page.sensor_graph_panes.length > 0
+          seq.data_set = seq.page.sensor_graph_panes.first.data_sets.first
+        end
+        seq.save
+      else
+        puts "#{seq.class.to_s} #{seq.id} has no associated page."
       end
-      seq.save
     end
   end
 
