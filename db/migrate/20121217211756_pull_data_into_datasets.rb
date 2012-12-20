354c8a9f2a1543b026427e7396a205145c1858c5
Parker Morse <flashesofpanic@gmail.com>
2012-12-20 11:15:19 -0500

    Updates migration for orphaned objects.
    [#40564705] Accounts for sequences with no remaining Page, or sequences whose Page doesn't belong to an Activity anymore.

--- db/migrate/20121217211756_pull_data_into_datasets.rb
+++ db/migrate/20121217211756_pull_data_into_datasets.rb
@@ -13,17 +13,21 @@ class PullDataIntoDatasets < ActiveRecord::Migration
       # Check for predefined graph panes, prediction graph panes, and senor graph panes in that order
       # Use their data set as the sequence data set if one is found.
       if seq.page
-        if seq.page.predefined_graph_panes.length > 0
-          seq.data_set = seq.page.predefined_graph_panes.first.data_sets.first
-        elsif seq.page.prediction_graph_panes.length > 0
-          seq.data_set = seq.page.prediction_graph_panes.first.data_sets.first
-        elsif seq.page.sensor_graph_panes.length > 0
-          seq.data_set = seq.page.sensor_graph_panes.first.data_sets.first
+        if seq.page.activity
+          if seq.page.predefined_graph_panes.length > 0
+            seq.data_set = seq.page.predefined_graph_panes.first.data_sets.first
+          elsif seq.page.prediction_graph_panes.length > 0
+            seq.data_set = seq.page.prediction_graph_panes.first.data_sets.first
+          elsif seq.page.sensor_graph_panes.length > 0
+            seq.data_set = seq.page.sensor_graph_panes.first.data_sets.first
+          end
+          # puts "#{seq.class.to_s} #{seq.id} is getting data_set #{seq.data_set ? seq.data_set.name : 'nil'}"
+          seq.save
+        # else
+        #   puts "#{seq.class.to_s} #{seq.id} belongs to a page with no activity."
         end
-        puts "#{seq.class.to_s} #{seq.id} is getting data_set #{seq.data_set ? seq.data_set.name : 'nil'}"
-        seq.save
-      else
-        puts "#{seq.class.to_s} #{seq.id} has no associated page."
+      # else
+      #   puts "#{seq.class.to_s} #{seq.id} has no associated page."
       end
     end
   end
