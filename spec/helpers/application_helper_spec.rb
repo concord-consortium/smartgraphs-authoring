6215f93bc0be8717570fb374e934978fd94357bf
Parker Morse <flashesofpanic@gmail.com>
2012-12-27 10:45:17 -0500

    Spec for helper method
    I don't know why this wasn't committed last week for [#37445673]. This is a partial spec for the helper method which determines which activity is currently being worked on.

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the ApplicationHelper. For example:
+#
+# describe ApplicationHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe ApplicationHelper do
+  describe 'activity_for' do
+    before(:all) do
+      @act = Activity.create!(:name => 'Activity to be found')
+      @page = Page.create!()
+      @page.activity = @act
+      @page.save
+    end
+
+    it 'returns an activity if there is a page_id' do
+      pending "This can't be effectively tested until I can supply params the helper can read"
+      controller.request = ActionController::TestRequest.new(:params => { :page_id => @page.id })
+      pp = PickAPointSequence.new()
+      helper.activity_for(pp).should == @act
+    end
+
+    it 'returns an activity if there is an activity_id' do
+      pending "This can't be effectively tested until I can supply params the helper can read"
+      controller.request = ActionController::TestRequest.new(:params => { :activity_id => @act.id })
+      pp = PickAPointSequence.new()
+      helper.activity_for(pp).should == @act
+    end
+
+    it 'returns an activity if the supplied argument responds to activity' do
+      pp = PickAPointSequence.new()
+      pp.page = @page
+      helper.activity_for(pp).should == @act
+    end
+
+    it 'returns nil if it has no way to find an activity' do
+      stub = Object.new()
+      helper.activity_for(pp).should === nil
+    end
+  end
+end
