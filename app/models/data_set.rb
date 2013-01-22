4afc68e03cf5c0be77cba85471ebafcd0ad50f86
Parker Morse <pmorse@concord.org>
2013-01-22 16:00:21 -0500

    Leaving these fields out of field_list in DataSet made all the tests break.

--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -49,7 +49,7 @@ class DataSet < ActiveRecord::Base
   validate :validate_expression
 
   def field_order
-    "name, expression, line_snap_distance, line_type, point_type, data, x_unit, y_unit"
+    "name, expression, line_snap_distance, line_type, point_type, data, x_unit, y_unit, x_precision, y_precision"
   end
 
   def to_hash
