cfa711452cf4f55d8eaa0dcacd51d7c76802cb2f
Richard Klancer <rpk@pobox.com>
2013-04-04 15:51:37 -0400

    Animations: return to activity after deleting
    [#47371327]

--- app/controllers/animations_controller.rb
+++ app/controllers/animations_controller.rb
@@ -5,4 +5,9 @@ class AnimationsController < ApplicationController
   auto_actions :all, :except => :index
   auto_actions_for :activity, [:new, :create]
 
+  def destroy
+  	animation = Animation.find params[:id]
+    hobo_destroy :redirect => animation.activity
+  end
+
 end
