5fd3acc840208592223ca2e95ff772ff33482439
Parker Morse <pmorse@concord.org>
2013-04-04 16:31:13 -0400

    Basic UI for adding LabelSequences to Pages.
    [#46033697] This ignores the question of the proper way to handle the LabelSet in question, which should actually be empty and invisible (because the student is going to fill it).

--- app/controllers/label_sequences_controller.rb
+++ app/controllers/label_sequences_controller.rb
@@ -2,6 +2,6 @@ class LabelSequencesController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :all
-
+  auto_actions :all, :except => :index
+  polymorphic_auto_actions_for :page, [ :index, :new, :create ]
 end
