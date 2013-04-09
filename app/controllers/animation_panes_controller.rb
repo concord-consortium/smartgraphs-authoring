97d38d8193993c977f5ea12290337e734e8ffe9c
Richard Klancer <rpk@pobox.com>
2013-04-09 15:18:10 -0400

    Redirect to page after destroying AnimationPane
    [#47371653]

--- app/controllers/animation_panes_controller.rb
+++ app/controllers/animation_panes_controller.rb
@@ -5,4 +5,9 @@ class AnimationPanesController < ApplicationController
   auto_actions :all, :except => :index
   polymorphic_auto_actions_for :page, [ :index, :new, :create ]
 
+  def destroy
+  	animation_pane = AnimationPane.find params[:id]
+    hobo_destroy :redirect => animation_pane.page
+  end
+
 end
