bf42d959bff7f62e9bc88fd9706af02e5921b9a7
Richard Klancer <rpk@pobox.com>
2013-04-09 15:58:49 -0400

    AnimationPane-related factories
    [#47371653]

--- spec/factories/animation_panes.rb
+++ spec/factories/animation_panes.rb
@@ -2,5 +2,18 @@
 
 FactoryGirl.define do
   factory :animation_pane do
+    after(:create) do |pane, evaluator|
+      pane.animation = FactoryGirl.create(:animation)
+    end
   end
+
+  factory :activity_with_animation_pane, :parent => :activity_with_animation do
+    after(:create) do |activity, evaluator|
+      activity.pages = FactoryGirl.create_list(:page, 1, :activity => activity)
+      page = activity.pages.first
+      animation = activity.animations.first
+      page.animation_panes = FactoryGirl.create_list(:animation_pane, 1, :page => page, :animation => animation)
+    end
+  end
+
 end
