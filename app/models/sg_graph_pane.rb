44624b3af15c7e2fa9edb3c58a0853b5b7a3944c
Parker Morse <pmorse@concord.org>
2013-04-10 10:17:55 -0400

    Output PDGP labels to hash [#46492277]

--- app/models/sg_graph_pane.rb
+++ app/models/sg_graph_pane.rb
@@ -85,12 +85,11 @@ module SgGraphPane
     if included_graphs.size > 0
       hash['includeAnnotationsFrom'] = included_graphs.map{ |graph| graph.get_indexed_path }
     end
-    begin
-      if label_sets.size > 0
+    if self.respond_to?(:label_sets) && label_sets.size > 0
         hash['labelSetNames'] = label_sets.map{ |label_set| label_set.name }
-      end
-    rescue NameError
-      # Not all GraphPanes have label_sets yet
+    end
+    if self.respond_to?(:graph_labels) && graph_labels.size > 0
+      hash['labels'] = graph_labels.map(&:to_hash)
     end
     hash
   end
