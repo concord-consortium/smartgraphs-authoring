0b55f904cb1774591482dda467a2820e94cdf02a
Parker Morse <pmorse@concord.org>
2013-04-10 09:40:51 -0400

    Looks like has_many not has_one

--- app/models/predefined_graph_pane.rb
+++ app/models/predefined_graph_pane.rb
@@ -66,7 +66,7 @@ class PredefinedGraphPane < ActiveRecord::Base
   has_many :label_sets, :through => :label_set_predefined_graphs
   has_many :label_set_predefined_graphs, :accessible => true, :dependent => :destroy
 
-  has_one :graph_label
+  has_many :graph_labels
 
   def field_order
     fo  = %w[title y_label y_min y_max y_ticks ]
