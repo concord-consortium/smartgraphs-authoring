5a6f1f2d9bf3c14895c501dfb268eb9eed03a739
Richard Klancer <rpk@pobox.com>
2013-04-17 11:19:14 -0400

    Serialization/copying support for EmptyPane
    [#47372055]

--- app/models/empty_pane.rb
+++ app/models/empty_pane.rb
@@ -3,6 +3,7 @@ class EmptyPane < ActiveRecord::Base
   hobo_model # Don't put anything above this
 
   include SgPermissions
+  include SgMarshal
   sg_parent :page
 
   fields do
@@ -13,5 +14,11 @@ class EmptyPane < ActiveRecord::Base
   has_one :page, :through => :page_pane
 
   reverse_association_of :page, 'Page#empty_panes'
+
+  def to_hash
+    {
+      'type' => 'EmptyPane'
+    }
+  end
 
 end
