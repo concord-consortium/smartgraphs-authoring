94cc74a3d17a2a30deeb69d6bb89933e29cfb140
Richard Klancer <rpk@pobox.com>
2013-04-17 11:17:42 -0400

    Redirect to page after destroying EmptyPane
    [#47372055]

--- app/controllers/empty_panes_controller.rb
+++ app/controllers/empty_panes_controller.rb
@@ -5,4 +5,9 @@ class EmptyPanesController < ApplicationController
   auto_actions :all, :except => :index
   polymorphic_auto_actions_for :page, [ :index, :new, :create ]
 
+  def destroy
+  	empty_pane = EmptyPane.find params[:id]
+    hobo_destroy :redirect => empty_pane.page
+  end
+
 end
