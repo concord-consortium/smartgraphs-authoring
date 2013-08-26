6974ed108b53b7b727e336c4b296f8535316383f
Parker Morse <pmorse@concord.org>
2013-08-26 15:35:22 -0400

    Disable AJAX for AnimationPane form
    [#52818341] Until I can get the AJAX refresh of this form working properly in Hobo, this version just disables it. This means we're providing form fields which are sometimes required and sometimes not, so obviously this isn't optimal, but at least it works.
    
    https://www.pivotaltracker.com/story/show/52818341

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-Event.addBehavior({
-    "select.animation-pane-animation:change": function (ev) {
-         Hobo.ajaxRequest(window.location.href, ['animation-part'], {
-             params: Form.serialize(this.up('form')),
-             method: 'get',
-             message: 'Please wait...'
-         });
-     }
-});
+// Event.addBehavior({
+//     "select.animation-pane-animation:change": function (ev) {
+//          Hobo.ajaxRequest(window.location.href, ['animation-part'], {
+//              params: Form.serialize(this.up('form')),
+//              method: 'get',
+//              message: 'Please wait...'
+//          });
+//      }
+// });
