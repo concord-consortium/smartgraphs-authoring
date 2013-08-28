5d5de468baf765f6ba0a087b7b842c26e3dbbde0
Parker Morse <pmorse@concord.org>
2013-08-28 11:40:17 -0400

    Fixes editing and page association. It works!
    \o/ [#52818341] AnimationPanes can be created by authors (non-admins) and are automatically associated with their pages. (This turned out to be the same problem: failing to associate the pane with the page meant the authoring algorithm couldn't tell if the user had permission to edit it.) The UI for editing the pane is the same as for creation, and panes created by the authoring tool run in the runtime.
    
    https://www.pivotaltracker.com/story/show/52818341

--- app/controllers/animation_panes_controller.rb
+++ app/controllers/animation_panes_controller.rb
@@ -18,13 +18,36 @@ class AnimationPanesController < ApplicationController
   # end
 
   def new_for_page
-    @animation_pane = AnimationPane.new
+    @animation_pane = AnimationPane.new(:page => Page.find(params[:page_id]))
     if params[:page_path]
       params[:page_path] = CGI.unescape(params[:page_path])
     end
     if request.xhr? && !params[:animation_id].blank?
       @animation_pane.animation = Animation.find(params[:animation_id])
       hobo_ajax_response
+    end
+  end
+
+  def edit
+    if params[:page_path]
+      params[:page_path] = CGI.unescape(params[:page_path])
+    end
+    hobo_edit do
+      if request.xhr? && !params[:animation_id].blank?
+        @animation_pane.animation = Animation.find(params[:animation_id])
+        hobo_ajax_response
+      end
+    end
+  end
+
+  def create
+    hobo_create do
+      if params[:page_path]
+        logger.info params.inspect
+        page = Page.find(params[:page_path].split('/')[2])
+        logger.info page.inspect
+        @animation_pane.page = page
+      end
     end
   end
 end
