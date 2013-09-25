e5bb1d2e9a5f02106baf77e2d958305e96e32371
Parker Morse <pmorse@concord.org>
2013-09-25 10:41:18 -0400

    Fixing permissions bug with creating panes
    [#57606942] See code comments: when a join model has no instances to join, return true for edit_permitted? instead of fruitlessly trying to find its parent.
    
    https://www.pivotaltracker.com/story/show/57606942

--- app/models/data_set_pane.rb
+++ app/models/data_set_pane.rb
@@ -5,8 +5,20 @@ class DataSetPane < ActiveRecord::Base
   # standard owner and admin permissions
   # defined in models/standard_permissions.rb
   include SgPermissions
-  sg_parent :page
-  
+  sg_parent :pane
+
+  def edit_permitted?(attribute)
+    # The sg_permissions.rb version of this wants to check the owner of its Activity.
+    # However, it checks before the join model is saved, so there is no sg_parent; neither
+    # parent nor pane, as it happens, which means the sg_parent process hits a nil and
+    # throws an exception. To which I say, holy cats, folks, it's just a join model.
+    if pane.blank? && data_set.blank?
+      return true
+    else
+      super(attribute)
+    end
+  end
+
   fields do
     in_legend :boolean, :default => false
     timestamps
