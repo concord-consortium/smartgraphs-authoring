61638f9259d67a4976963b56f8b7d2ebadf5bc1b
Noah Paessel <knowuh@gmail.com>
2014-07-11 16:30:07 -0400

    Update capistrano tasks to point at AWS servers.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,4 @@
 #############################################################
-#  Connection:
-#############################################################
-set :gateway, "seymour.concord.org"
-
-#############################################################
 #  Application
 #############################################################
 
@@ -14,6 +9,6 @@ set :branch, "staging"
 #  Servers
 #############################################################
 
-set :domain, "ruby-vm8.concord.org"
+set :domain, "smartgraphs-authoring.staging.concord.org"
 server domain, :app, :web
 role :db, domain, :primary => true
